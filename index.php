b3f0dc24e153bac7e4c6eecd9235b70a4158d051
FrankZiWANG-dev <wang.frank.zi@gmail.com>
2021-08-09 13:36:32 +0200

    no phpmailer

--- index.php
+++ index.php
@@ -64,34 +64,7 @@ if(filter_has_var(INPUT_GET, 'submit')){
         
             //validate
             if (filter_var(trim($email), FILTER_VALIDATE_EMAIL) == true){
-
-                  //Create an instance; passing `true` enables exceptions
-                $mail = new PHPMailer(true);
-
-                if ($mail->send()) {
-                    //Server settings
-                    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-                    $mail->isSMTP();                                            //Send using SMTP
-                    $mail->Host       = 'localhost';                     //Set the SMTP server to send through
-                    $mail->Port       = 25;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-                    //Recipients
-                    $mail->setFrom("wang.frank.zi@gmail.com");
-                    $mail->addAddress($mail);     //Add a recipient
-                    $mail->addReplyTo("wang.frank.zi@gmail.com");
-
-                    //Content
-                    $mail->isHTML(true);                                  //Set email format to HTML
-                    $mail->Subject = "Thank you for your message!";
-                    $mail->Body    = "<h2> Thank you for reaching out! </h2>\r\n <p>We'll get back to you ASAP</p>";
-                    $mail->AltBody = "Thanks for reaching out! We'll get back to you asap!";
-
-                    $mail->send();
-                    echo 'Message has been sent';
-                } 
-                else {
-                    echo "Message could not be sent.";
-                }
+                echo "e-mail is valid";
             }
         }
     }  
